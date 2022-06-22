a6cdbd96834e52adfd1ae570d8b59f085d69722a
kasturi28 <kasturipansare2818@gmail.com>
2022-06-22 18:44:53 +0530

    display welcome message

---
+++ src/linecomparison2/LineComparison.java
@@ -0,0 +1,12 @@
+package linecomparison2;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+
+		System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAM ON MAIN  BRANCH");
+		// TODO Auto-generated method stub
+
+	}
+
+}
