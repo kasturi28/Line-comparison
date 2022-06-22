cd8180b960ad7d7d7ae18aaf474a6edbad88792c
kasturi28 <kasturipansare2818@gmail.com>
2022-06-22 22:42:40 +0530

    UC1_added_Calculation_LengthOfLine

--- src/linecomparison2/LineComparison.java
+++ src/linecomparison2/LineComparison.java
@@ -1,11 +1,25 @@
 package linecomparison2;
 
+
 public class LineComparison {
 
 	public static void main(String[] args) {
 
 		System.out.println("WELCOME TO LINE COMPARISON COMPUTATION PROGRAM ON MAIN  BRANCH");
-		// TODO Auto-generated method stub
+		
+		
+		// defining coordinates of the line
+		int x1 = 4;
+        int y1 = 5;
+        int x2 = 6;
+        int y2 = 7;
+        
+        //calculating the length
+        System.out.println("Coordinates x1 and y1 of first point is "+x1+", "+y1);
+        System.out.println("Coordinates x2 and y2 of second point is "+x2+", "+y2);
+
+        double length1 = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        System.out.println("Length pf line1 is "+length1);
 
 	}
 
